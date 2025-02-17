3f69c35dc960da18d2759f2940e9ef1f2b0c1d75
Gergely Wellner <gergely.wellner.ext@porscheinformatik.com>
2025-02-17 08:48:39 +0100

    Only use helm for helm yaml and lsp only for yaml which is not helm.

--- .config/nvim/lua/plugins/helm.lua
+++ .config/nvim/lua/plugins/helm.lua
@@ -2,10 +2,33 @@ local configs = require("lspconfig.configs")
 local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 
+-- https://github.com/LazyVim/LazyVim/discussions/2281
+-- Disable yamlls for helm fileype so that yamlls is not loaded when edditing a helm chart.
+
+require("lazyvim.util").lsp.on_attach(function(client, buffer)
+  if client.name == "yamlls" then
+    if vim.api.nvim_buf_get_option(buffer, "filetype") == "helm" then
+      vim.schedule(function()
+        vim.cmd("LspStop ++force yamlls")
+      end)
+    end
+  end
+end)
+
 -- vim-helm
 return {
-  -- ft = "helm",
+  ft = "helm",
   "towolf/vim-helm",
+
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        helm_ls = {},
+        yamlls = {},
+      },
+    },
+  },
   config = function()
     if not configs.helm_ls then
       configs.helm_ls = {
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,49 @@
+-- https://github.com/Jomik/dotfiles/blob/718c8f82963dfc0f0a1c4c25725b75223024a68c/.config/nvim/lua/plugins/lang/yaml.lua
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "yaml" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+      version = false, -- last release is way too old
+    },
+    opts = {
+      -- make sure mason installs the server
+      ---@type lspconfig.options
+      servers = {
+        yamlls = {
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
+            vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas())
+          end,
+          settings = {
+            yaml = {
+              format = { enable = true },
+              hover = true,
+              validate = true,
+              keyOrdering = false,
+              -- https://docs.gitlab.com/ee/ci/yaml/yaml_optimization.html#reference-tags
+              customTags = {
+                "!reference sequence",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
