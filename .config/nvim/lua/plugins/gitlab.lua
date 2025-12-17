ee33fb9f6ff76e6cc0183e6424ca5cf092e5be2e
Gergely Wellner <gergely.wellner.ext@porscheinformatik.com>
2025-12-17 14:24:30 +0100

    Added gitlab plugin and polybar vpn config

---
+++ .config/nvim/lua/plugins/gitlab.lua
@@ -0,0 +1,104 @@
+return {
+  {
+    "harrisoncramer/gitlab.nvim",
+    lazy = true,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      -- "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+      -- "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+    },
+    build = function()
+      require("gitlab.server").build(true)
+    end, -- Builds the Go binary
+    config = function()
+      require("gitlab").setup({
+        discussion_signs = {
+          virtual_text = true, -- Whether to show the comment text inline as floating virtual text
+        },
+      })
+    end,
+
+    keys = {
+      {
+        "<leader>tg",
+        function()
+          require("gitlab")
+        end,
+        desc = "Code Review GitLab",
+      },
+      {
+        "<leader>tgl",
+        desc = "Gitlab list merge requests",
+        function()
+          require("gitlab").choose_merge_request()
+        end,
+      },
+
+      {
+        "<leader>tgr",
+        function()
+          require("gitlab").review()
+        end,
+        desc = "Git lab review",
+      },
+      {
+        "<leader>tgc",
+        function()
+          require("gitlab").create_comment()
+        end,
+        desc = "Gitlab create comment",
+        mode = "n",
+      },
+      {
+        "<leader>tgm",
+        function()
+          require("gitlab").create_mr()
+        end,
+        desc = "Create a merge request",
+        mode = "n",
+      },
+      {
+        "<leader>tgm",
+        function()
+          require("gitlab").perform_action("create_mr")
+        end,
+        desc = "Create MR (ZZ commit)",
+        mode = "n",
+      },
+      {
+
+        "<leader>tgA",
+        function()
+          require("gitlab").add_assignee()
+        end,
+        desc = "Add Assigne",
+        mode = "n",
+      },
+      {
+        "<leader>tgc",
+        function()
+          require("gitlab").create_multiline_comment()
+        end,
+        desc = "Gitlab create comment",
+        mode = "v",
+      },
+      {
+        "<leader>tgs",
+        function()
+          require("gitlab").create_comment_suggestion()
+        end,
+        desc = "Gitlab create comment suggestion",
+        mode = "v",
+      },
+      {
+        "<leader>tga",
+        function()
+          require("gitlab").approve()
+        end,
+        desc = "Gitlab Approve",
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -40,7 +40,7 @@ return {
       require("dbee").install()
     end,
     config = function()
-      require("dbee").setup(--[[optional config]] { lazy = true })
+      require("dbee").setup(--[[optional config]] { lazy = true, log_level = "debug" })
     end,
     init = function()
       require("which-key").add({
@@ -226,124 +226,51 @@ return {
       },
     },
   },
-  {
-    "harrisoncramer/gitlab.nvim",
-    lazy = true,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      -- "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
-      -- "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
-    },
-    build = function()
-      require("gitlab.server").build(true)
-    end, -- Builds the Go binary
-    config = function()
-      require("gitlab").setup({
-        discussion_signs = {
-          virtual_text = true, -- Whether to show the comment text inline as floating virtual text
-        },
-      })
-    end,
-
-    keys = {
-      {
-        "<leader>tg",
-        function()
-          require("gitlab")
-        end,
-        desc = "Code Review GitLab",
-      },
-      {
-        "gl",
-        desc = "Git lab list merge requests",
-      },
-      -- {
-      --   "<leader>tgr",
-      --   function()
-      --     require("gitlab").review()
-      --   end,
-      --   desc = "Git lab review",
-      -- },
-      -- {
-      --   "<leader>tgc",
-      --   function()
-      --     require("gitlab").create_comment()
-      --   end,
-      --   desc = "Gitlab create comment",
-      --   mode = "n",
-      -- },
-      -- {
-      --   "<leader>tgc",
-      --   function()
-      --     require("gitlab").create_multiline_comment()
-      --   end,
-      --   desc = "Gitlab create comment",
-      --   mode = "v",
-      -- },
-      -- {
-      --   "<leader>tgs",
-      --   function()
-      --     require("gitlab").create_comment_suggestion()
-      --   end,
-      --   desc = "Gitlab create comment suggestion",
-      --   mode = "v",
-      -- },
-      -- {
-      --   "<leader>tga",
-      --   function()
-      --     require("gitlab").approve()
-      --   end,
-      --   desc = "Gitlab Approve",
-      -- },
-    },
-  },
-  {
-    "R-nvim/R.nvim",
-    -- Only required if you also set defaults.lazy = true
-    lazy = false,
-    -- R.nvim is still young and we may make some breaking changes from time
-    -- to time (but also bug fixes all the time). If configuration stability
-    -- is a high priority for you, pin to the latest minor version, but unpin
-    -- it and try the latest version before reporting an issue:
-    -- version = "~0.1.0"
-    config = function()
-      -- Create a table with the options to be passed to setup()
-      ---@type RConfigUserOpts
-      local opts = {
-        hook = {
-          on_filetype = function()
-            vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-            vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
-          end,
-        },
-        R_args = { "--quiet", "--no-save" },
-        min_editor_width = 72,
-        rconsole_width = 78,
-        objbr_mappings = { -- Object browser keymap
-          c = "class", -- Call R functions
-          ["<localleader>gg"] = "head({object}, n = 15)", -- Use {object} notation to write arbitrary R code.
-          v = function()
-            -- Run lua functions
-            require("r.browser").toggle_view()
-          end,
-        },
-        disable_cmds = {
-          "RClearConsole",
-          "RCustomStart",
-          "RSPlot",
-          "RSaveClose",
-        },
-      }
-      -- Check if the environment variable "R_AUTO_START" exists.
-      -- If using fish shell, you could put in your config.fish:
-      -- alias r "R_AUTO_START=true nvim"
-      if vim.env.R_AUTO_START == "true" then
-        opts.auto_start = "on startup"
-        opts.objbr_auto_start = true
-      end
-      require("r").setup(opts)
-    end,
-  },
+  -- {
+  --   "R-nvim/R.nvim",
+  --   -- Only required if you also set defaults.lazy = true
+  --   lazy = false,
+  --   -- R.nvim is still young and we may make some breaking changes from time
+  --   -- to time (but also bug fixes all the time). If configuration stability
+  --   -- is a high priority for you, pin to the latest minor version, but unpin
+  --   -- it and try the latest version before reporting an issue:
+  --   -- version = "~0.1.0"
+  --   config = function()
+  --     -- Create a table with the options to be passed to setup()
+  --     ---@type RConfigUserOpts
+  --     local opts = {
+  --       hook = {
+  --         on_filetype = function()
+  --           vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+  --           vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+  --         end,
+  --       },
+  --       R_args = { "--quiet", "--no-save" },
+  --       min_editor_width = 72,
+  --       rconsole_width = 78,
+  --       objbr_mappings = { -- Object browser keymap
+  --         c = "class", -- Call R functions
+  --         ["<localleader>gg"] = "head({object}, n = 15)", -- Use {object} notation to write arbitrary R code.
+  --         v = function()
+  --           -- Run lua functions
+  --           require("r.browser").toggle_view()
+  --         end,
+  --       },
+  --       disable_cmds = {
+  --         "RClearConsole",
+  --         "RCustomStart",
+  --         "RSPlot",
+  --         "RSaveClose",
+  --       },
+  --     }
+  --     -- Check if the environment variable "R_AUTO_START" exists.
+  --     -- If using fish shell, you could put in your config.fish:
+  --     -- alias r "R_AUTO_START=true nvim"
+  --     if vim.env.R_AUTO_START == "true" then
+  --       opts.auto_start = "on startup"
+  --       opts.objbr_auto_start = true
+  --     end
+  --     require("r").setup(opts)
+  --   end,
+  -- },
 }
